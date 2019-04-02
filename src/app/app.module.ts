16ba72fd820e6eb7acf8e2d96c142643e0596971
chisomo <chisomo9@gmail.com>
2019-04-02 16:27:17 +0200

    component template v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DailyQuizComponent } from './daily-quiz/daily-quiz.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DailyQuizComponent,
+    LeaderboardComponent
   ],
   imports: [
     BrowserModule
