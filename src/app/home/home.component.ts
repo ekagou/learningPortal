3d1e8e0dcfa765f912e5ef456b92e1eee2bc2a1a
System Administrator <root@rfc.private.address.invalid.query>
2019-04-03 06:40:48 +0200

    update rest call

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,7 @@ import { ProductService } from '../services/product.service';
 })
 export class HomeComponent implements OnInit {
   username = '';
+  id = "";
   response: any;
   constructor(private svc: ProductService, private http: HttpClient) {
     this.svc.printToConsole('Got the service');
@@ -19,7 +20,18 @@ export class HomeComponent implements OnInit {
   }
 
   search() {
-    this.http.get('https://jsonplaceholder.typicode.com/users/' + this.username).
+    if(this.username == "premier"){
+      this.id = "100";
+    }
+    if (this.username == "private") {
+      this.id = "200";
+    } 
+    if (this.username == "wealth") {
+      this.id = "300";
+    } 
+    console.log(this.username);
+    //this.http.get('https://jsonplaceholder.typicode.com/users/' + this.id).
+    this.http.get('https://api.mlab.com/api/1/databases/codefest/collections/products/'+this.id+'?apiKey=zzoeSGcNjMLtwaPPxqOukd4Jllas2BTs').
       subscribe((response) => {
         this.response = response;
         console.log(response);
